328661dd1ca3449819809da8cee6921c81a5adaa
Nam Nguyen <namnx.vn@gmail.com>
2026-07-09 13:48:21 +0700

    feat: export React state intelligence rules

---
+++ src/react/rules/state/index.ts
@@ -0,0 +1,4 @@
+export { reactStateDerivedStateRule } from "./derived-state";
+export { reactStateMutationRule } from "./mutation";
+export { reactStateRedundantStateRule } from "./redundant-state";
+export { reactStateSynchronizationRule } from "./synchronization";
