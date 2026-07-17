1487e54541a4a6638c3582e863fd487654750e7f
Nam Nguyen <namnx.vn@gmail.com>
2026-07-17 16:21:37 +0700

    feat(security): export shared taint flow primitives

---
+++ src/analyzer/security/flow/index.ts
@@ -0,0 +1,11 @@
+export { analyzeIntraproceduralTaint } from "./intraprocedural-taint";
+export type {
+  TaintFlowAdapter,
+  TaintFlowMatch,
+  TaintKind,
+  TaintSanitizer,
+  TaintSink,
+  TaintSource,
+  TaintState,
+  TaintStep,
+} from "./types";
