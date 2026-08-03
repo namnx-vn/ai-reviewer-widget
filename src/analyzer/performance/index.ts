7a40c8e7b13a68f0fc3a59660f069b421e6cff9f
Namnx <namnx.vn@gmail.com>
2026-08-03 15:31:26 +0700

    feat(performance): add interprocedural summaries

--- src/analyzer/performance/index.ts
+++ src/analyzer/performance/index.ts
@@ -2,5 +2,6 @@ export { PerformanceAnalysisEngine } from "./engine/performance-analysis-engine"
 export { createPerformanceFindingId } from "./engine/finding-id";
 export { PerformanceRuleRegistry } from "./registry/performance-rule-registry";
 export { analyzePerformanceCosts } from "./cost/cost-propagation";
+export { analyzeInterproceduralPerformance } from "./interprocedural";
 export { assetPerformanceRules, asyncPerformanceRules, backpressurePerformanceRules, cachePerformanceRules, cpuPerformanceRules, databasePerformanceRules, importPerformanceRules, loadingPerformanceRules, memoryPerformanceRules, resiliencePerformanceRules } from "./rules";
 export type * from "./model/types";
