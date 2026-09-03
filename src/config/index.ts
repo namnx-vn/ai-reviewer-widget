cc302ac88e8a6073b521f9539a29eb0982577485
Nam Nguyen <namnx.vn@gmail.com>
2026-09-03 16:37:30 +0700

    feat(config): export project profile API

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,4 +14,14 @@ export type {
 } from "./contracts";
 export { DEFAULT_REVIEW_CONFIGURATION, DEFAULT_RULE_CATALOG } from "./defaults";
 export { isPathIncluded } from "./paths";
+export {
+  PROJECT_PROFILES,
+  resolveProjectProfiles,
+} from "./project-profiles";
+export type {
+  ProjectProfileEvidence,
+  ProjectProfileId,
+  ProjectProfileResolution,
+  ProjectProfileSignals,
+} from "./project-profiles";
 export { parseReviewConfiguration, resolveReviewConfiguration } from "./resolve";
