1e49b2e894ef0894798631cde5dff5b016d54a4b
Nam Nguyen <namnx.vn@gmail.com>
2026-09-06 03:10:20 +0700

    feat(performance): export test-file policy

--- src/analyzer/performance/policies/index.ts
+++ src/analyzer/performance/policies/index.ts
@@ -1,2 +1,3 @@
 export { applyPerformanceProfile, PERFORMANCE_PROFILE_DEFINITIONS, resolvePerformanceProfile } from "./performance-profiles";
 export type { PerformanceProfileId, ResolvedPerformanceProfile } from "./performance-profiles";
+export { isPerformanceTestFile, shouldRunPerformanceRuleForFile } from "./test-file-policy";
